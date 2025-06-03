837b3f07ecc3e8e9dae0075f8e2c941c33c8e882
Sannebeast <jimk0001@hz.nl>
2025-06-03 11:47:19 +0200

    installed laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,43 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-/*
- * The home routes
+ * The home route - shows tasks index page
  */
 Route::get('/', [TaskController::class, 'index'])->name('home');
 
 /*
- * The resource routes
+ * Resource routes for tasks, posts, projects
  */
-Route::resource('tasks', TaskController::class)
-    ->except(['index']); // removed this route because this is moved to the home route
-Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])
-    ->name('tasks.complete');
+Route::resource('tasks', TaskController::class)->except(['index']);
+Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
 Route::resource('posts', PostController::class);
 Route::resource('projects', ProjectController::class);
 
 /*
- * Route that shows the about page. This handler just returns the about view.
+ * Static about page
  */
 Route::view('/about', 'about')->name('about');
+
+/*
+ * Dashboard route (requires authentication)
+ */
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+/*
+ * Routes requiring authentication
+ */
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
